0a8e5750191477e40c12d835b594d0b32de3518e
Santiago Sierra Cano <santiscano@gmail.com>
2023-03-08 16:00:30 -0500

    form create user

--- src/Layout/Ti/Submits.ts
+++ src/Layout/Ti/Submits.ts
@@ -1,4 +1,6 @@
 import { createRol } from "../../services/Roles.routes";
+import { createCedi } from "../../services/Cedis.routes";
+import { createUser } from '../../services/Users.routes'
 
 
 export const handleSubmitCreateRol = async (
@@ -18,14 +20,71 @@ export const handleSubmitCreateRol = async (
   } finally {}
 };
 
-export const handleSubmitCreateCedi = async () => {
-  try{} catch(error) {
+export const handleSubmitCreateCedi = async (
+  e:any,
+  city:string,
+  setCity: any,
+  country:string,
+  setCountry: any,
+  address:string,
+  setAddress: any,
+  cediName:string,
+  setCediName: any,
+  type:string,
+  setType: any,
+) => {
+  try{
+    e.preventDefault();
+    const res = await createCedi(city, country, address, cediName, type)
+    console.log('res create cedis: ', res);
+    setCity('');
+    setCountry('');
+    setAddress('');
+    setCediName('');
+    setType('');
+  } catch(error) {
     console.log('error: ', error);
   } finally{}
 }
 
-export const handleSubmitCreateUser = async () => {
-  try{} catch(error) {
+export const handleSubmitCreateUser = async (
+  e:any,
+  idroles: number,
+  setIdroles:any,
+  idsedes: number,
+  setIdsedes: any,
+  identification_type: string,
+  setIdentification_type: any,
+  identification_number: string,
+  setIdentification_number: any,
+  firstname: string,
+  setFirstname: any,
+  lastname: string,
+  setLastname: any,
+  address: string,
+  setAddress: any,
+  phone: string,
+  setPhone: any,
+  email: string,
+  setEmail: any,
+  password: string,
+  setPassword: any,
+) => {
+  try{
+    e.preventDefault();
+    const res = await createUser(idroles, idsedes, identification_type, identification_number, firstname, lastname,address,phone, email,password)
+    console.log('res createUser: ', res);
+    setIdroles('');
+    setIdsedes('');
+    setIdentification_type('');
+    setIdentification_number('');
+    setFirstname('');
+    setLastname('');
+    setAddress('');
+    setPhone('');
+    setEmail('');
+    setPassword('');
+  } catch(error) {
     console.log('error: ', error);
   } finally{}
 }
