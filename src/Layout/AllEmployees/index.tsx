b63c55e8425877d3c26a31d4cae1149903b69f1e
Santiago Sierra Cano <santiscano@gmail.com>
2023-03-31 11:17:35 -0500

    version 1.0.0

--- src/Layout/AllEmployees/index.tsx
+++ src/Layout/AllEmployees/index.tsx
@@ -63,29 +63,29 @@ function AllEmployees({
   //   });
   // };
   return (
-    <div onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
-      <h1>hola mundo desde allEmployees</h1>
-      <h3>
-        crear tabla con todos los empleados y que se pueda filtrar por nombre,
-        cc. etc
-      </h3>
-      <div style={{ position: "absolute", left: position.x, top: position.y }}>
-        <Document>
-          <Page size={{ width: 189, height: 94.5 }}>
-            <View>
-              <Image src={LOGO} style={styles.image} />
-              <Text style={styles.text}>{cediType}</Text>
-              <Text style={styles.text}>{settledNumber}</Text>
-              <Text style={styles.text}>{accountType}</Text>
-              <Text style={styles.text}>Mensaje de prueba</Text>
-            </View>
-          </Page>
-        </Document>
-      </div>
-      <div style={{ position: "relative" }}>
-        <iframe src={requerimientos} />
-      </div>
-    </div>
+    // <div onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
+    //   <h1>hola mundo desde allEmployees</h1>
+    //   <h3>
+    //     crear tabla con todos los empleados y que se pueda filtrar por nombre,
+    //     cc. etc
+    //   </h3>
+    //   <div style={{ position: "absolute", left: position.x, top: position.y }}>
+    //     <Document>
+    //       <Page size={{ width: 189, height: 94.5 }}>
+    //         <View>
+    //           <Image src={LOGO} style={styles.image} />
+    //           <Text style={styles.text}>{cediType}</Text>
+    //           <Text style={styles.text}>{settledNumber}</Text>
+    //           <Text style={styles.text}>{accountType}</Text>
+    //           <Text style={styles.text}>Mensaje de prueba</Text>
+    //         </View>
+    //       </Page>
+    //     </Document>
+    //   </div>
+    //   <div style={{ position: "relative" }}>
+    //     <iframe src={requerimientos} />
+    //   </div>
+    // </div>
 
     <div>
       <div
